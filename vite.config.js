f3cb1638d42bb3d2f99ef50bc3fcdd7fc8bdee30
claudinomil <claudinomoraes@yahoo.com.br>
2026-09-21 16:09:54 -0300

    Missões 21/09/2026 10

--- vite.config.js
+++ vite.config.js
@@ -10,16 +10,20 @@
 //     ],
 // });
 
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig(({ mode }) => ({
-    base: mode === 'production' ? '/dgf_sistema/' : '/',
+export default defineConfig({
+    base: './',
 
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
-}));
+});
